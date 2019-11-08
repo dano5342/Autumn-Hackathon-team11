13a52a232bb43c7dfb0175cd1c5fcf3748433d9f
Daniel Ellis <ellis.daniel0@gmail.com>
2019-11-08 14:20:34 +0000

    Added bootstrap, links to styles etc

---
+++ assets/scripts/style.js
@@ -0,0 +1,58 @@
+
+
+(function(){
+    var allQuestions = [
+        {question: "This is the question",
+        answers:["a:", "b:", "c:"]
+        },
+        {question: "This is the question",
+        answers:["a:", "b:", "c:"]
+        },
+        {question: "This is the question",
+        answers:["a:", "b:", "c:"]
+        },
+        {question: "This is the question",
+        answers:["a:", "b:", "c:"]
+        },
+        {question: "This is the question",
+        answers:["a:", "b:", "c:"]
+        },
+        {question: "This is the question",
+        answers:["a:", "b:", "c:"]
+        },
+        {question: "This is the question",
+        answers:["a:", "b:", "c:"]
+        },
+        {question: "This is the question",
+        answers:["a:", "b:", "c:"]
+        },
+        {question: "This is the question",
+        answers:["a:", "b:", "c:"]
+        },
+        {question: "This is the question",
+        answers:["a:", "b:", "c:"]
+        }];
+
+    questionCounter = 0;
+    selectOptions = [];
+    areaQuiz = $('#quiz');
+
+    nextQuestion();
+
+    $('#next').click(function(){
+        chooseOption();
+        if (isNaN(selectOptions[questionCounter])){
+            alert('Find out your fate...');
+        }
+        else {
+            questionCounter++;
+            nextQuestion();
+        }
+    });
+
+    $('#prev').click(function() {
+        chooseOption();
+        quesCounter--;
+        nextQuestion();
+    });
+}
