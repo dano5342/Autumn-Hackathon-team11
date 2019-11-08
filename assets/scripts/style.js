72c63b8867500c4458b781882cdadbdf84140895
Daniel Ellis <ellis.daniel0@gmail.com>
2019-11-08 15:34:12 +0000

    JS Completed, now to work on styling and html

--- assets/scripts/style.js
+++ assets/scripts/style.js
@@ -1,122 +1,149 @@
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
 
-    var myQuestions = [
-        {
-            question: "This is the question",
+    var myQuestions = [{
+            question: "Where was Dracula born?",
             answers: {
-                a: 'False',
-                b: 'False',
-                c: 'True'
+                a: 'Slovakia'
+                b: 'Romania'
+                c: 'Hungary'
             },
             correctAnswer: 'b'
         },
         {
-            question: "This is the question",
+            question: "In which area did Dracula live?",
             answers: {
-                a: 'False',
-                b: 'False',
-                c: 'True'
+                a: 'Tiraspol'
+                b: 'Targu-Mures'
+                c: 'Transylvania'
+            },
+            correctAnswer: 'c'
+        },
+        {
+            question: "What is the name of the famous Dracula Castle?",
+            answers: {
+                a: 'Bran Castle',
+                b: 'Stone Castle',
+                c: 'Vajahunyad Castle'
+            },
+            correctAnswer: 'a'
+        },
+        {
+            question: "How old is the Dracula Castle estimated to be?",
+            answers: {
+                a: '520 years old',
+                b: '642 years old',
+                c: '846 years old'
             },
             correctAnswer: 'b'
         },
         {
-            question: "This is the question",
+            question: "In which part of the body does Vampires attack?",
             answers: {
-                a: 'False',
-                b: 'False',
-                c: 'True'
+                a: 'Right-arm',
+                b: 'Neck',
+                c: 'Heart'
             },
             correctAnswer: 'b'
         },
         {
-            question: "This is the question",
+            question: "What are the vampires strengths?",
             answers: {
-                a: 'False',
-                b: 'False',
-                c: 'True'
+                a: 'Intelligent, fast, seducers',
+                b: 'Impervious to gunshot',
+                c: 'Garlic farming'
+            },
+            correctAnswer: 'a'
+        },
+        {
+            question: "How old is Dracula estimated to be if he's still alive?",
+            answers: {
+                a: '659 Y/O',
+                b: '588 Y/O',
+                c: '749 Y/O'
             },
             correctAnswer: 'b'
         },
         {
-            question: "This is the question",
+            question: "When was the first Vampire movie made?",
             answers: {
-                a: 'False',
-                b: 'False',
-                c: 'True'
+                a: '1953',
+                b: '1965',
+                c: '1920'
+            },
+            correctAnswer: 'c'
+        },
+        {
+            question: "What happens when you get bitten in your sleep?",
+            answers: {
+                a: 'You never wake up again.',
+                b: 'A transformation begins',
+                c: 'Slight headache when you wakeup'
             },
             correctAnswer: 'b'
         },
         {
-            question: "This is the question",
+            question: "How do you kill vampires?",
             answers: {
-                a: 'False',
-                b: 'False',
-                c: 'True'
+                a: 'Using blessed silver bullets and garlic',
+                b: 'Pouring water over the vampire',
+                c: 'Throwing him some apples'
             },
-            correctAnswer: 'b'
-        },
-        {
-            question: "This is the question",
-            answers: {
-                a: 'False',
-                b: 'False',
-                c: 'True'
-            },
-            correctAnswer: 'b'
-        },
-        {
-            question: "This is the question",
-            answers: {
-                a: 'False',
-                b: 'False',
-                c: 'True'
-            },
-            correctAnswer: 'b'
-        },
-        {
-            question: "This is the question",
-            answers: {
-                a: 'False',
-                b: 'False',
-                c: 'True'
-            },
-            correctAnswer: 'b'
-        },
-        {
-            question: "This is the question",
-            answers: {
-                a: 'False',
-                b: 'False',
-                c: 'True'
-            },
-            correctAnswer: 'b'
+            correctAnswer: 'a'
         },
     ];
 
-	function showQuestions(questions, quizContainer){
-		// code will go here
-	}
+    function showQuestions(questions, quizContainer) {
+        var output = [];
+        var answers;
+        for (var i = 0; i < questions.length; i++) {
+            for (letter in questions[i].answers) {
+                answers.push(
+                    '<label>' +
+                    '<input type="radio" name="question' + i + '" value="' + letter + '">' +
+                    letter + ': ' +
+                    questions[i].answers[letter] +
+                    '</label>'
 
-	function showResults(questions, quizContainer, resultsContainer){
-		// code will go here
-	}
-
-	// show the questions
-	showQuestions(questions, quizContainer);
-
-	// when user clicks submit, show results
-    // Incorporate an if loop for this part,
-    /*
-    if (myquestions.answer == 'a' || 'b'){
-        alert(YOU DIED)
-    }else{
-
+                );
+            }
+            output.push(
+                '<div class="question">' + questions[i].question + '</div>' +
+                '<div class="answers">' + answers.join('') + '</div>'
+            );
+        }
     }
-    */
-	submitButton.onclick = function(){
-		showResults(questions, quizContainer, resultsContainer);
-	}
 }
 
+function showResults(questions, quizContainer, resultsContainer) {
 
+    var answerContainers = quizContainer.querySelectorAll('.answers');
 
+    var userAnswer = '';
+    var numCorrect = 0;
+
+    for (var i = 0; i < questions.length; i++) {
+        userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked') || {}).value;
+        if (userAnswer === questions[i].correctAnswer) {
+            numCorrect++;
+
+            answerContainers[i].style.color = 'lightgreen';
+        }
+        else {
+            answerContainers[i].style.color = 'red';
+        }
+    }
+
+    resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+}
+showQuestions(questions, quizContainer);
+
+submitButton.onclick = function() {
+    showResults(questions, quizContainer, resultsContainer);
+}
+}
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
